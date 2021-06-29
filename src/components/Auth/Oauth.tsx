b7c33a190c97484fdaecde749bcf16ed120b8b1d
Serg <umt1@ukr.net>
2021-06-29 19:10:53 +0300

    222

--- src/components/Auth/Oauth.tsx
+++ src/components/Auth/Oauth.tsx
@@ -21,7 +21,8 @@ const Oauth: FunctionComponent<OauthProps> = ({
   useEffect(() => onSetToken && onSetToken(token));
   useEffect(() => onSetProfile && onSetProfile(token));
 
-  return <Redirect to={ROUTE_URLS.DASHBOARD} />;
+  // return <Redirect to={ROUTE_URLS.DASHBOARD} />;
+  return <h2>TEST {token}</h2>;
 };
 
 const mapDispatchToProps = (dispatch: any) => {
